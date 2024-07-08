3e4f59a35d1b1ca3c746c462c2936670d64bbe9c
LeeJiho <leejiho35@gmail.com>
2024-07-08 20:50:51 +0900

    :sparkles: : add dashboard ui

--- src/pages/dashboard-page/index.tsx
+++ src/pages/dashboard-page/index.tsx
@@ -1,25 +1,61 @@
+import { useState } from "react";
 import { Header } from "../../components/header";
 import tw from "twin.macro";
 
 const DashBoard = () => {
+  const [balance, setBalance] = useState(0);
+
+  const handleCheckBalance = () => {
+    setBalance(100);
+  };
+
+  const dummyLiquidity = [
+    {
+      token: "ETH",
+      amount: 0,
+    },
+    {
+      token: "USDT",
+      amount: 0,
+    },
+    {
+      token: "USDC",
+      amount: 0,
+    },
+  ];
   return (
     <Wrapper>
       <Header />
       <Container>
-        <ContentWrapper>
-          <ContentTitle>Check My Balance at FuGazi</ContentTitle>
-          <ContentSubTitle>
-            FuGazi is a service that allows you to swap tokens on the Helium
-            network.
-          </ContentSubTitle>
-        </ContentWrapper>
-        <ContentWrapper>
+        <BalanceWrapper>
+          <BalanceDescription>
+            <ContentTitle>Check My Balance at FuGazi</ContentTitle>
+            <ContentSubTitle>
+              FuGazi is a service that allows you to swap tokens on the Helium
+              network.
+            </ContentSubTitle>
+          </BalanceDescription>
+          <BalanceButtonWrapper>
+            <StyledButton onClick={handleCheckBalance}>
+              Check My Balance
+            </StyledButton>
+            <MyBalance>My Balance : {balance}</MyBalance>
+          </BalanceButtonWrapper>
+        </BalanceWrapper>
+        <LiquidityWrapper>
           <ContentTitle>My Liquidity Pool</ContentTitle>
           <ContentSubTitle>
             FuGazi is a service that allows you to swap tokens on the Helium
             network.
           </ContentSubTitle>
-        </ContentWrapper>
+
+          {dummyLiquidity.map((liquidity, index) => (
+            <Liquidity key={index}>
+              <LiquidityTitle>{liquidity.token}</LiquidityTitle>
+              <LiquidityAmount>{liquidity.amount}</LiquidityAmount>
+            </Liquidity>
+          ))}
+        </LiquidityWrapper>
         <ContentWrapper>
           <ContentTitle>Claimed Balance</ContentTitle>
           <ContentSubTitle>
@@ -48,9 +84,49 @@ const Container = tw.div`
   p-48 gap-24
 `;
 
+const BalanceWrapper = tw.div`
+  flex gap-8 p-16 w-600
+  bg-green-1
+`;
+
+const BalanceDescription = tw.div`
+  flex flex-col gap-4
+`;
+
+const StyledButton = tw.button`
+  bg-green-2 hover:bg-green-3 text-white font-semibold h-36
+  px-16 py-2 rounded-md 
+  border-none
+`;
+
+const MyBalance = tw.div`
+  font-l-m
+`;
+
+const BalanceButtonWrapper = tw.div`
+  flex flex-col items-center gap-8
+`;
+
+const LiquidityWrapper = tw.div`
+  flex flex-col gap-8 p-16 w-600
+  bg-green-1
+`;
+
+const Liquidity = tw.div`
+  flex items-center p-8 gap-8 bg-green-2
+`;
+
+const LiquidityTitle = tw.div`
+  font-l-m
+`;
+
+const LiquidityAmount = tw.div`
+  font-l-m
+`;
+
 const ContentWrapper = tw.div`
   flex flex-col gap-8 p-16 w-600
-  bg-green-2
+  bg-green-1
 `;
 
 const ContentTitle = tw.div`
