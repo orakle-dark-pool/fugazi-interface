668235f962e625ebc51aec48b8a6d4b29513b44b
LeeJiho <leejiho35@gmail.com>
2024-07-31 17:16:29 +0900

    :sparkles: : dashboard page ui

--- src/pages/dashboard-page/index.tsx
+++ src/pages/dashboard-page/index.tsx
@@ -8,6 +8,8 @@ import { useViewer } from "../../contract/viewer";
 import { usePoolActionFacet } from "../../contract/pool-action-facet";
 import { FUGAZI_ADDRESS, USD_ADDRESS } from "../../assets/address";
 import { useAccountContract } from "../../contract/account";
+import usdLogo from "../../assets/usd.png";
+import fgzLogo from "../../assets/logo.png";
 
 const DashBoard = () => {
   const [balance, setBalance] = useState(0);
@@ -55,12 +57,33 @@ const DashBoard = () => {
 
   const dummyLiquidity = [
     {
-      token: "FGZ",
+      token1: "FGZ",
+      token2: "USD",
+      token1Logo: fgzLogo,
+      token2Logo: usdLogo,
       amount: 0,
     },
     {
-      token: "USD",
+      token1: "FGZ",
+      token2: "USD",
+      token1Logo: fgzLogo,
+      token2Logo: usdLogo,
       amount: 0,
+    },
+  ];
+
+  const dummyOrders = [
+    {
+      Pair: "FGZ-USD",
+      Epoch: 1,
+      Type: "Buy",
+      Time: "2021-01-01 12:00:00",
+    },
+    {
+      Pair: "FGZ-USD",
+      Epoch: 2,
+      Type: "Sell",
+      Time: "2021-01-01 12:00:00",
     },
   ];
 
@@ -107,10 +130,33 @@ const DashBoard = () => {
             </BalanceButtonWrapper>
           </TextWrapper>
           <ClaimImageWrapper>
-            <ClaimImage src={claim1} alt="claim-1" />
+            <ClaimImage src={claim1} alt="claim" />
             <ClaimButton onClick={handleClaimOrder}>Claim</ClaimButton>
           </ClaimImageWrapper>
         </BalanceWrapper>
+
+        <LiquidityWrapper>
+          <ContentTitle>LP Balances</ContentTitle>
+
+          {dummyLiquidity.map((liquidity, index) => (
+            <Liquidity key={index}>
+              <LiquidityPair>
+                <TokenLogo src={liquidity.token1Logo} alt="token-logo" />
+                <TokenLogo src={liquidity.token2Logo} alt="token-logo" />
+              </LiquidityPair>
+              <LiquidityTitle>
+                {liquidity.token1} - {liquidity.token2}
+              </LiquidityTitle>
+              <LiquidityAmount>
+                {liquidity.amount} {liquidity.token1}
+              </LiquidityAmount>
+              <LiquidityAmount>
+                {liquidity.amount} {liquidity.token2}
+              </LiquidityAmount>
+              <WithdrawButton>Withdraw</WithdrawButton>
+            </Liquidity>
+          ))}
+        </LiquidityWrapper>
 
         <ContentWrapper>
           <ContentTitle>Withdraw Balance</ContentTitle>
@@ -130,27 +176,21 @@ const DashBoard = () => {
           </WithdrawInputWrapper>
         </ContentWrapper>
 
-        <LiquidityWrapper>
-          <ContentTitle>My Liquidity Pool</ContentTitle>
-          <ContentSubTitle>
-            FuGazi is a service that allows you to swap tokens on the Helium
-            network.
-          </ContentSubTitle>
-
-          {dummyLiquidity.map((liquidity, index) => (
-            <Liquidity key={index}>
-              <LiquidityTitle>{liquidity.token}</LiquidityTitle>
-              <LiquidityAmount>{liquidity.amount}</LiquidityAmount>
-            </Liquidity>
-          ))}
-        </LiquidityWrapper>
-
         <ContentWrapper>
-          <ContentTitle>Transaction History</ContentTitle>
+          <ContentTitle>My Orders</ContentTitle>
           <ContentSubTitle>
             FuGazi is a service that allows you to swap tokens on the Helium
             network.
           </ContentSubTitle>
+          {dummyOrders.map((order, index) => (
+            <Order key={index}>
+              <OrderText>Pair : {order.Pair}</OrderText>
+              <OrderText>Epoch : {order.Epoch}</OrderText>
+              <OrderText>Type : {order.Type}</OrderText>
+              <OrderText>Time : {order.Time}</OrderText>
+              <OrderButton>Settle & Claim</OrderButton>
+            </Order>
+          ))}
         </ContentWrapper>
       </Container>
     </Wrapper>
@@ -220,16 +260,30 @@ const ClaimButton = tw.button`
 `;
 
 const LiquidityWrapper = tw.div`
-  flex flex-col gap-8 p-16 w-600
+  flex flex-col gap-8 p-16 w-800
   bg-green-1
 `;
 
 const Liquidity = tw.div`
-  flex items-center p-8 gap-8 bg-green-2
+  flex items-center justify-between p-8 gap-8 bg-green-2
+`;
+
+const LiquidityPair = tw.div`
+  flex items-center gap-8
+`;
+
+const TokenLogo = tw.img`
+  w-30 h-30
 `;
 
 const LiquidityTitle = tw.div`
   font-l-m
+`;
+
+const WithdrawButton = tw.button`
+  bg-green-2 hover:bg-green-3 text-white font-semibold h-36 w-150
+  px-16 py-2 rounded-md 
+  border-solid border-green-3 border-2
 `;
 
 const LiquidityAmount = tw.div`
@@ -237,7 +291,7 @@ const LiquidityAmount = tw.div`
 `;
 
 const ContentWrapper = tw.div`
-  flex flex-col gap-16 p-16 w-600
+  flex flex-col gap-16 p-16 w-800
   bg-green-1
 `;
 
@@ -256,6 +310,24 @@ const ClaimWrapper = tw.div`
 
 const ClaimImage = tw.img`
   w-300 h-300
+`;
+
+const Order = tw.div`
+  flex items-center justify-between p-8 gap-8 bg-green-2
+`;
+
+const OrderButton = tw.button`
+  bg-green-2 hover:bg-green-3 text-white font-semibold h-36 w-150
+  px-16 py-2 rounded-md 
+  border-solid border-green-3 border-2
+`;
+
+const OrderText = tw.div`
+  font-l-m
+`;
+
+const OrderEpoch = tw.div`
+  font-l-m
 `;
 
 export default DashBoard;
